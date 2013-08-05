457ecd6311825030c1055f4ca13505b0e3d4ce74
Mike Dory <mike@dory.me>
2013-08-05 01:25:42 -0400

    ah, and removing my todo-comments

--- scraper.py
+++ scraper.py
@@ -1,19 +1,3 @@
-# scrape at x interval
-#   this might be easier to do with celery beat, actually
-# 
-# look for this word
-#   requests
-#       beautiful soup
-# 
-# if it's changed, do whatever
-#   check against redis
-#       if same, move on
-#       if different, store, with timestamp
-# 
-# use difflib.unified_diff to calculate
-# 
-# Mandrill to send
-
 # import the usual stuff
 import time
 import difflib
