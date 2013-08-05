e3b9c7ece25beb0846225ca87808a8c99a91b1db
Mike Dory <mike@dory.me>
2013-08-05 14:12:59 -0400

    notes!

--- celery/celeryconfig.py
+++ celery/celeryconfig.py
@@ -1,10 +1,13 @@
-from datetime import timedelta
+# from datetime import timedelta
 
-''' 
+""" 
 set up the backend defaults for the results and broker
 
 based heavily on: https://gist.github.com/vivekn/1062526
-'''
+
+TODO:   move this stuff into local_settings
+
+"""
 
 CELERY_RESULT_BACKEND = "redis"
 REDIS_HOST = "localhost"
@@ -16,20 +19,21 @@ BROKER_HOST = "localhost"  # Maps to redis host.
 BROKER_PORT = 6379         # Maps to redis port.
 BROKER_VHOST = "0"         # Maps to database number.
  
-CELERY_IMPORTS = ("module.submodule.foo", ) # Module which contains the tasks you want to call asynchronously
+# CELERY_IMPORTS = ("module.submodule.foo", ) # Module which contains the tasks you want to call asynchronously
 
 
-'''
+"""
 set up the scheduler
-'''
+
+"""
 
 
-CELERYBEAT_SCHEDULE = {
-    'add-every-30-seconds': {
-        'task': 'tasks.add',
-        'schedule': timedelta(seconds=30),
-        'args': (16, 16)
-    },
-}
+# CELERYBEAT_SCHEDULE = {
+#     'add-every-30-seconds': {
+#         'task': 'tasks.add',
+#         'schedule': timedelta(seconds=30),
+#         'args': (16, 16)
+#     },
+# }
 
-CELERY_TIMEZONE = 'UTC'
+# CELERY_TIMEZONE = 'UTC'
