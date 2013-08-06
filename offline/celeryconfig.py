5c13716361a5b68bc197c4c45c0f7c9a82bca081
Mike Dory <mike@dory.me>
2013-08-06 17:41:57 -0400

    revising how arguments get passed to the tasks

--- offline/celeryconfig.py
+++ offline/celeryconfig.py
@@ -6,9 +6,8 @@ import local_settings
 
 
 """ 
-set up the backend defaults for the results and broker
-
-based heavily on: https://gist.github.com/vivekn/1062526
+Set up the backend defaults for the results and broker
+(based heavily on: https://gist.github.com/vivekn/1062526)
 
 TODO:   move this stuff into local_settings
 
@@ -25,17 +24,36 @@ CELERY_IMPORTS = ("offline.tasks", ) # Module which contains the tasks you want 
 
 
 """
-set up the scheduler
+Set up the scheduler, which will then tell celery beat how often to run, and
+what command to run when invoked.
+
+Start the worker:
+
+    celery -A offline.tasks worker --config=offline.celeryconfig --loglevel=info
+
+Start the scheduler:
+
+    celery beat --config="offline.celeryconfig" \
+        --loglevel="info" --schedule="/tmp/celerybeat_schedule.db"
+
+Rather than running workers and the `beat` task, run a single worker in beat mode:
+
+    celery -A offline.tasks worker --config=offline.celeryconfig --loglevel=info -B
 
 """
 
+# define the target to hit
+target_url = local_settings.TARGET_URL
+target_element_name = local_settings.TARGET_ELEMENT_NAME
 
+# schedulize it
 CELERYBEAT_SCHEDULE = {
     'scrape-site': {
         'task': 'offline.tasks.scrape',
         'schedule': timedelta(seconds=10),
-        'args': ()
+        'args': (target_url, target_element_name)
     },
 }
 
+# timezone (UTC ALL THE THINGS!)
 CELERY_TIMEZONE = 'UTC'
