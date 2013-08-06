e5f06b674c8ea123cabc768de09104d6238f9bef
Mike Dory <mike@dory.me>
2013-08-06 17:42:14 -0400

    reshaping the tasks to accept arguments from celery beat

--- offline/tasks.py
+++ offline/tasks.py
@@ -1,6 +1,5 @@
 import logging
 
-import local_settings
 from scraper import Scraper
 
 from celery import Celery
@@ -12,7 +11,7 @@ celery.config_from_object('offline.celeryconfig')
 
 
 @celery.task
-def scrape():
+def scrape(target_url, target_element_name):
     """
     Create a task that Celery can run to scrape and update all the things.
 
@@ -23,10 +22,6 @@ def scrape():
 
     # debug, if desired
     logging.debug('Using config file %s' % offline.celeryconfig.__name__)
-
-    # define the target to hit
-    target_url = local_settings.TARGET_URL
-    target_element_name = local_settings.TARGET_ELEMENT_NAME
 
     target_content = scraper.fetch_site_content(target_url, target_element_name)
     cached_content = scraper.fetch_cache()
