1159723c04eae79391089d6f3799191090bbc34e
Mike Dory <mike@dory.me>
2013-08-06 17:32:19 -0400

    breaking the cache and database bits out into their own modules

---
+++ scraper.py
@@ -0,0 +1,71 @@
+# import our favorite libraries
+import requests
+from bs4 import BeautifulSoup
+
+# import our local stuffs
+import local_settings
+
+
+# the scraper class. the brains of the organization, really.
+class Scraper:
+    """
+    Provides methods for scraping target pages and comparing their content
+    against cached, earlier versions.
+
+    TODO:   rename this class. and this file, probably.
+
+    """
+
+    def __init__(self):
+        """
+        Initalize all our lovely constants and stuff, and create a 
+        db connection object for later use.
+
+        """
+        # set the search parameter constants
+        self.target_url = local_settings.TARGET_URL
+        self.target_element_name = local_settings.TARGET_ELEMENT_NAME
+
+
+    def fetch_site_content(self, target_url, target_element_name='html'):
+        """
+        Fetches the content of the requested site, based on element types.
+        It uses CSS selectors (jQuery-style), and defaults to `html`
+        if not otherwise explicitly set. 
+        
+        """
+        # snag the page content
+        try:
+            # attempt to request and parse the target content
+            r = requests.get(target_url)
+            soup = BeautifulSoup(r.text)
+
+            # find all the divs matching the requested selectors
+            target_html = soup.select(target_element_name)
+            target_content = target_html[0].prettify(formatter="html")
+
+        # if the request fails, return None
+        except requests.exceptions.ConnectionError:
+            target_content = None
+
+        return target_content
+
+
+# if we're here to run, run it
+if __name__ == "__main__":
+    """
+    When run on its own, the scraper will go fetch the target content
+    and print it out for you.
+
+    """
+
+    # initialize the scraper
+    scraper = Scraper()
+
+    # define yer targets
+    target_url = local_settings.TARGET_URL
+    target_element_name = local_settings.TARGET_ELEMENT_NAME
+
+    # scrape stuff!
+    target_content = scraper.fetch_site_content(target_url, target_element_name)
+    print target_content
